07ef3bd9c58743721f9feb178c5a047601a56e57
Alexis Rondeau <alexis.rondeau@gmail.com>
2017-04-30 15:15:50 +0200

    Bootstraps app with RSpec

---
+++ spec/features/prioritization_feature_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'Prioritization' do
+  describe 'seeing the list of tasks' do
+    it 'lets me see the list of tasks' do
+      # TODO: Build the app
+    end
+  end
+end
